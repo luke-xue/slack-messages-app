871af08509cec2f43dcdacb6853c1bb1a8b3e8fe
Luke Xue <luqi9900@gmail.com>
2023-06-16 01:25:20 -0400

    event setup

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { App } = require('@slack/bolt');
-console.log(process.env.SLACK_BOT_TOKEN)
+const { App } = require("@slack/bolt");
+console.log(process.env.SLACK_BOT_TOKEN);
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
