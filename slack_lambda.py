49d63455f8905357600b145d606a58e867b8f41e
Luke Xue <luqi9900@gmail.com>
2023-06-25 17:12:24 -0400

    response to slack

--- slack_lambda.py
+++ slack_lambda.py
@@ -1,6 +1,15 @@
+import json
+
 def lambda_handler(event, context):
-    body = event.get('body')
-    # if body["type"] == "url_verification":
-    return body
-    # else:
-    #     return None
+    body = json.loads(event.get('body'))
+    if body["type"] == "url_verification":
+        return body
+    elif body["type"] == "event_callback":
+        return handle_callback(body)
+    else:
+        return None
+    
+def handle_callback(body):
+    if body["event"]["type"] == "message":
+        print(body["event"]["text"])
+    return None
