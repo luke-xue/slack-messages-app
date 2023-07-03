97c888e9aa88e35e337cb952bd1d028c975343e0
Luke Xue <luqi9900@gmail.com>
2023-07-03 03:20:50 -0400

    sheets API call working

--- slack_lambda.py
+++ slack_lambda.py
@@ -2,7 +2,16 @@ import json
 import os
 from slack_sdk import WebClient
 from datetime import datetime
+import pytz
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
@@ -22,5 +31,54 @@ def handle_callback(body):
         user = client.users_info(user=user_id)
         print(body["event"])
         print(user)
-        print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+        print(datetime.fromtimestamp(float(ts)).strftime('%Y-%m-%d %H:%M:%S'))
     return None
+
+def add_message_to_sheet(spreadsheet_id, timestamp, name, message):
+    creds, _ = google.auth.default()
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        requests = []
+        requests.append({
+            'appendCells': {
+                "sheetId": os.environ['SHEET_ID'],
+                "rows": [
+                    {
+                    "values": [
+                        {
+                        "userEnteredValue": {
+                            "stringValue": timestamp
+                            }
+                        },
+                        {
+                        "userEnteredValue": {
+                            "stringValue": name
+                            }
+                        },
+                        {
+                        "userEnteredValue": {
+                            "stringValue": message
+                            }
+                        }
+                    ]
+                    }
+                ],
+                "fields": "userEnteredValue"
+                }
+        })
+
+        body = {
+            'requests': requests
+        }
+        response = service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body=body).execute()
+        return response
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+add_message_to_sheet(os.environ['SPREADSHEET_ID'],"time","name","text")
